e13adfaae50a70f4f910f57493463d8c69d8a786
lathesh rai <latheshraikumbra@gmail.com>
2024-11-14 18:41:12 +0530

    contactus

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import SupplierDetails from './components/SupplierDetails'
 import Contactus from './components/Contactus'
+import Updateproduct from './components/Updateproduct'
+import Addproduct from './components/Addproduct'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +16,8 @@ function App() {
 
         <Route path="/supplierdetail" element={<SupplierDetails />} />
         <Route path="/contactus" element={<Contactus />} />
-        
+        <Route path="/updateproduct" element={<Updateproduct />} />
+        <Route path="/Addproduct" element={<Addproduct />} />
         </Routes>
       </Router>
     </>
