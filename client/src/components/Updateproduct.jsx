3fbe2b653475d3afa7200e52afc7358ac5de9a39
backend <shubhamprabhu1001@gmail.com>
2024-11-15 07:01:21 +0530

    merge added

--- client/src/components/Updateproduct.jsx
+++ client/src/components/Updateproduct.jsx
@@ -83,27 +83,27 @@ function UpdateProduct() {
         <div className="update-product-wrapper">
             <div className="product-form-container">
                 <h2 className="product-form-title">Update Your Product</h2>
-                <form className="product-form" onSubmit={handleSubmit}>
+                <form className="product-form" onSubmit={handleSubmit} >
                     <label className="product-form-label">Product Name
-                        <input type="text" name="productname" value={product.productname} onChange={handleChange} className="product-form-input" required />
+                        <input type="text" name="productname" value={product.productname} onChange={handleChange} disabled className="product-form-input" required />
                     </label>
                     <label className="product-form-label">Quantity
-                        <input type="number" name="quantity" value={product.quantity} onChange={handleChange} className="product-form-input" required />
+                        <input type="number" name="quantity" value={product.quantity} onChange={handleChange} className="product-form-input" disabled required />
                     </label>
                     <label className="product-form-label">Price
-                        <input type="number" name="price" value={product.price} onChange={handleChange} className="product-form-input" required />
+                        <input type="number" name="price" value={product.price} onChange={handleChange} className="product-form-input" disabled required />
                     </label>
                     <label className="product-form-label">Supplier Name
-                        <input type="text" name="supplierName" value={product.supplierName} onChange={handleChange} className="product-form-input" required />
+                        <input type="text" name="supplierName" value={product.supplierName} onChange={handleChange} disabled className="product-form-input" required />
                     </label>
                     <label className="product-form-label">Total Sold
-                        <input type="number" name="total_sold" value={product.total_sold} onChange={handleChange} className="product-form-input" required />
+                        <input type="number" name="total_sold" value={product.total_sold} onChange={handleChange} disabled className="product-form-input" required />
                     </label>
                     <label className="product-form-label">Revenue Generated
-                        <input type="number" name="revenue_generated" value={product.revenue_generated} onChange={handleChange} className="product-form-input" required />
+                        <input type="number" name="revenue_generated" value={product.revenue_generated} onChange={handleChange} disabled className="product-form-input" required />
                     </label>
                     <label className="product-form-label">Threshold Quantity
-                        <input type="number" name="productthreshold" value={product.productthreshold} onChange={handleChange} className="product-form-input" required />
+                        <input type="number" name="productthreshold" value={product.productthreshold} onChange={handleChange} disabled  className="product-form-input" required />
                     </label>
                     <button type="submit" className="product-form-submit">Update</button>
                 </form>
